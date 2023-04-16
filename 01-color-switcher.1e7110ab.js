09b54ddd3ee4163d94424d545926c7f3915f669b
IlliaKomissarov <116258677+IlliaKomissarov@users.noreply.github.com>
2023-04-16 10:02:50 +0000

    Deploying to gh-pages from @ IlliaKomissarov/goit-js-hw-09@4e50ae2249a7aa95c5edd6881b7a818c11f9876d 🚀

--- 01-color-switcher.1e7110ab.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button.data-start"),e=document.querySelector("button.data-stop");document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),e.removeAttribute("disabled"),setInterval((function(){var t=getRandomHexColor();bodyElem.style.backgroundColor=t}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.1e7110ab.js.map
--- 01-color-switcher.479bf430.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button.data-start"),e=document.querySelector("button.data-stop");document.querySelector("body");let o=null;t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),e.removeAttribute("disabled"),o=setInterval((()=>{const t=getRandomHexColor();bodyElem.style.backgroundColor=t}),1e3)}));
-//# sourceMappingURL=01-color-switcher.479bf430.js.map
